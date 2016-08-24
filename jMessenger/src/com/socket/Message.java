47d8629e3e834d25f2118bb0f63088afa347d29e
matthew <matthew@DESKTOP-ILR55EU>
2016-08-24 17:51:13 +0800

    1st commit

---
+++ jMessenger/src/com/socket/Message.java
@@ -0,0 +1,21 @@
+package com.socket;
+
+import java.io.Serializable;
+
+public class Message implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    public String type, sender, recipient, content;
+    
+    public Message(String type, String sender, String content, String recipient){
+        this.type = type; 
+        this.sender = sender; 
+        this.recipient = recipient;
+        this.content = content; 
+    }
+    
+    @Override
+    public String toString(){
+        return "{type='"+type+"', sender='"+sender+"', content='"+content+"', recipient='"+recipient+"'}";
+    }
+}
